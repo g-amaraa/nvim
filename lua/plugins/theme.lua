8df67e5a3a61a0291eabc3a71dc994842819d1d1
g-amaraa <79859757+g-amaraa@users.noreply.github.com>
2025-09-03 08:04:17 +0800

    Update theme.lua

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,10 +6,10 @@
 -- ================================================================================================
 
 return {
-	"savq/melange-nvim",
+	"rebelot/kanagawa.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme melange")
+		vim.cmd("colorscheme kanagawa")
 	end,
 }
