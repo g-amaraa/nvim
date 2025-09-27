008ba275c4ef8f7dc73e30ccf1fcdb1683e1a210
g-amaraa <79859757+g-amaraa@users.noreply.github.com>
2025-09-27 12:14:10 +0800

    Update tailwindcss.lua

--- lua/servers/tailwindcss.lua
+++ lua/servers/tailwindcss.lua
@@ -8,8 +8,8 @@
 --- @param capabilities table LSP client capabilities (typically from nvim-cmp or similar)
 --- @param on_attach function Callback function executed when LSP attaches to a buffer
 --- @return nil
-return function(lspconfig, capabilities, on_attach)
-	lspconfig.tailwindcss.setup({
+return function(capabilities, on_attach)
+	vim.lsp.config.tailwindcss = {
 		capabilities = capabilities,
 		on_attach = on_attach,
 		filetypes = {
@@ -20,5 +20,5 @@ return function(lspconfig, capabilities, on_attach)
 			"vue",
 			"svelte",
 		},
-	})
+	}
 end
