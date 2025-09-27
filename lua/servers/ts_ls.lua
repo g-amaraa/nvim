995338a0b3858c3d7638cfbd5b4ed020e3c8948d
g-amaraa <79859757+g-amaraa@users.noreply.github.com>
2025-09-27 12:14:46 +0800

    Update ts_ls.lua

--- lua/servers/ts_ls.lua
+++ lua/servers/ts_ls.lua
@@ -8,8 +8,8 @@
 --- @param capabilities table LSP client capabilities (typically from nvim-cmp or similar)
 --- @param on_attach function Callback function executed when LSP attaches to a buffer
 --- @return nil
-return function(lspconfig, capabilities, on_attach)
-	lspconfig.ts_ls.setup({
+return function(capabilities, on_attach)
+	vim.lsp.config.ts_ls = {
 		on_attach = on_attach,
 		capabilities = capabilities,
 		filetypes = {
@@ -24,5 +24,5 @@ return function(lspconfig, capabilities, on_attach)
 				indentSize = 2,
 			},
 		},
-	})
+	}
 end
