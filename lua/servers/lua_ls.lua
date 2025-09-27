056e9c7769c4d31b0f8021c23f3d2c55433b8005
g-amaraa <79859757+g-amaraa@users.noreply.github.com>
2025-09-27 12:11:44 +0800

    Update lua_ls.lua

--- lua/servers/lua_ls.lua
+++ lua/servers/lua_ls.lua
@@ -8,8 +8,8 @@
 --- @param capabilities table LSP client capabilities (typically from nvim-cmp or similar)
 --- @param on_attach function Callback function executed when LSP attaches to a buffer
 --- @return nil
-return function(lspconfig, capabilities, on_attach)
-	lspconfig.lua_ls.setup({
+return function(capabilities, on_attach)
+	lspconfig.lua_ls = {
 		capabilities = capabilities,
 		on_attach = on_attach,
 		settings = {
@@ -25,5 +25,5 @@ return function(lspconfig, capabilities, on_attach)
 				},
 			},
 		},
-	})
+	}
 end
