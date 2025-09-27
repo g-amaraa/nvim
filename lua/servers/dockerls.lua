3111ba298bc51bad5f1713c7db016050cf833a12
g-amaraa <79859757+g-amaraa@users.noreply.github.com>
2025-09-27 12:06:39 +0800

    Update dockerls.lua

--- lua/servers/dockerls.lua
+++ lua/servers/dockerls.lua
@@ -8,10 +8,10 @@
 --- @param capabilities table LSP client capabilities (typically from nvim-cmp or similar)
 --- @param on_attach function Callback function executed when LSP attaches to a buffer
 --- @return nil
-return function(lspconfig, capabilities, on_attach)
-	lspconfig.dockerls.setup({
+return function(capabilities, on_attach)
+	vim.lsp.config.dockerls = {
 		capabilities = capabilities,
 		on_attach = on_attach,
 		filetypes = { "dockerfile" },
-	})
+	}
 end
