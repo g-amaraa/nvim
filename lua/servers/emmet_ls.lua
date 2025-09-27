af7423007ec4e53eeb3d8b42ba948288089e8b1c
g-amaraa <79859757+g-amaraa@users.noreply.github.com>
2025-09-27 12:07:59 +0800

    Update emmet_ls.lua

--- lua/servers/emmet_ls.lua
+++ lua/servers/emmet_ls.lua
@@ -9,8 +9,8 @@
 --- @param capabilities table LSP client capabilities (typically from nvim-cmp or similar)
 --- @param on_attach function Callback function executed when LSP attaches to a buffer
 --- @return nil
-return function(lspconfig, capabilities, on_attach)
-	lspconfig.emmet_ls.setup({
+return function(capabilities, on_attach)
+	vim.lsp.config.emmet_ls = {
 		capabilities = capabilities,
 		on_attach = on_attach,
 		filetypes = {
@@ -24,5 +24,5 @@ return function(lspconfig, capabilities, on_attach)
 			"svelte",
 			"vue",
 		},
-	})
+	}
 end
