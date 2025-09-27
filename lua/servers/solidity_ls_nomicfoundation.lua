13a2152b692f48bfc2045380ced99d942e06cc0a
g-amaraa <79859757+g-amaraa@users.noreply.github.com>
2025-09-27 12:13:40 +0800

    Update solidity_ls_nomicfoundation.lua

--- lua/servers/solidity_ls_nomicfoundation.lua
+++ lua/servers/solidity_ls_nomicfoundation.lua
@@ -8,13 +8,13 @@
 --- @param capabilities table LSP client capabilities (typically from nvim-cmp or similar)
 --- @param on_attach function Callback function executed when LSP attaches to a buffer
 --- @return nil
-return function(lspconfig, capabilities, on_attach)
-	lspconfig.solidity.setup({
+return function(capabilities, on_attach)
+	vim.lsp.config.solidity = {
 		capabilities = capabilities,
 		on_attach = on_attach,
 		single_file_support = true,
 		cmd = { "nomicfoundation-solidity-language-server", "--stdio" },
 		filetypes = { "solidity" },
 		root_dir = lspconfig.util.find_git_ancestor,
-	})
+	}
 end
