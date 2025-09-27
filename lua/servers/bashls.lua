d19e3dd0c9547cbc4082ac5522f892648ee41c15
g-amaraa <79859757+g-amaraa@users.noreply.github.com>
2025-09-27 12:05:26 +0800

    Update bashls.lua

--- lua/servers/bashls.lua
+++ lua/servers/bashls.lua
@@ -8,10 +8,10 @@
 --- @param capabilities table LSP client capabilities (typically from nvim-cmp or similar)
 --- @param on_attach function Callback function executed when LSP attaches to a buffer
 --- @return nil
-return function(lspconfig, capabilities, on_attach)
-	lspconfig.bashls.setup({
+return function(capabilities, on_attach)
+	vim.lsp.config.bashls = {
 		capabilities = capabilities,
 		on_attach = on_attach,
 		filetypes = { "sh", "bash", "zsh" },
-	})
+	}
 end
